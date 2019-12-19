3cf67c33ca5fa40069440473ae4d50e8c262d1b3
cdbishop <creyavaichxxi@gmail.com>
2019-12-19 12:26:21 +0000

    EventManager: remove old code

--- src/System/EventManager.cpp
+++ src/System/EventManager.cpp
@@ -14,11 +14,6 @@ EventManager::~EventManager()
 void EventManager::Update()
 {
   auto now = EventManager::Clock::now();
-  /*for (auto&& event : _after_events) {
-    if (now >= event.first) {
-      event.second();
-    }
-  }*/
 
   _after_events.erase(std::remove_if(_after_events.begin(), _after_events.end(), [&now](EventManager::AfterPair event) {
     if (now >= event.first) {
